835988d6eda87bf337a2f7f7d249e637cce359fd
+ <+>
2024-01-24 17:52:13 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value;
+
+    console.log(taskDescription);
+
+    
+    taskInput.value = '';
+  });
 });
